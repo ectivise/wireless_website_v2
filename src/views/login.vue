68046c536f5a08fae7f66f309d76cbb41911fd9c
Melvin Chua <chua0946@e.ntu.edu.sg>
2020-06-01 18:47:06 +0800

    login page directed to ap

--- src/views/login.vue
+++ src/views/login.vue
@@ -5,9 +5,23 @@
 
 <script>
 export default {
+  data(){
+    return {
+      login:this.$store.state.login
+    }
+  },
+  mounted(){
+    this.goto_accesspoint()
+  },
+  methods:{
+    goto_accesspoint(){
+      if(this.login == true){
+        let url = "/accesspoint";
+        this.$router.push(url);
+      }
+    },
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
